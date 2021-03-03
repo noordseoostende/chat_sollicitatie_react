4775132db374f9d17199b2c5c634689759c131ad
noordseoostende <58941189+noordseoostende@users.noreply.github.com>
2021-03-03 22:32:51 +0100

    messageForm

---
+++ src/components/ChatFeed.jsx
@@ -0,0 +1,22 @@
+import MessageForm from './MessageForm';
+import MyMessage from './MyMessage';
+import TheirMessage from './TheirMessage';
+
+const ChatFeed = (props) => {
+    const { chats, activeChat, userName, messages } = props;
+    const chat = chats && chats[activeChat];
+    const renderMessages = () => (
+        const keys = Object.keys(messages);
+        return keys.map((key, index)=> {
+            const message = messages[key];
+        })
+    )
+    renderMessages()
+    return (
+        <div>
+            ChtFeed
+        </div>
+    )
+}
+
+export default ChatFeed;

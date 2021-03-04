89f6179f855dfa88dfba8e8295ca0aa71b886b1e
noordseoostende <58941189+noordseoostende@users.noreply.github.com>
2021-03-04 17:59:21 +0100

    3 undefined

--- src/components/ChatFeed.jsx
+++ src/components/ChatFeed.jsx
@@ -1,33 +1,46 @@
-import MessageForm from './MessageForm';
 import MyMessage from './MyMessage';
 import TheirMessage from './TheirMessage';
+import MessageForm from './MessageForm';
 
 const ChatFeed = (props) => {
     const { chats, activeChat, userName, messages } = props;
     const chat = chats && chats[activeChat];
-    const renderMessages = () => (
+    const renderReadReceipts = (message, isMyMessage) => {
+        return chat.people.map((person, index) => person.last_read == message.id && (
+            <div 
+                key={`read_${index}`}
+                className="read-receipt"
+                style={{
+                    float: isMyMessage ? 'right' : 'left',
+                    backgroundImage: `url(${person?.person?.avatar})`
+                }}
+            />
+        ))
+    }
+    const renderMessages = () => {
         const keys = Object.keys(messages);
+
         return keys.map((key, index)=> {
             const message = messages[key];
-            const lastMessageKey = index == 0 ? null : keys[index - 1];
-            const isMyMesage = userName == message.sender.username;
+            const lastMessageKey = index === 0 ? null : keys[index - 1];
+            const isMyMesage = userName === message.sender.username;
             return (
                 <div key={`msg_${index}`} style={{ width: '100%' }}>
                     <div className="message-block">
                         {
-                            isMessage
+                            isMyMessage
                             ? <MyMessage message={message} />
-                            : <TheirMessage message={message} lastMessage={messages[lastMessageKey]} />
-                        }
-                        <div className="read-receipts" style={{ marginRight: isMessage ? '18px' : '0px', marginLeft: isMyMesage ? '0px' : '68px'}}>
-                        read-receipts
-
+                            : <TheirMessage message={message} lastMessage={messages[lastMessageKey]} />}
                         </div>
+                        <div className="read-receipts" style={{ marginRight: isMessage ? '18px' : '0px', marginLeft: isMyMesage ? '0px' : '68px'}}>
+                        {renderReadReceipts(message, isMyMesage)}
+
+                        
                     </div>
                 </div>
             );
         })
-    )
+    }
     
 
     if(!chat) return 'Loading...';
--- src/components/MyMessage.jsx
+++ src/components/MyMessage.jsx
@@ -2,7 +2,7 @@ const MyMessage = ({ message }) => {
     if(message?.attachments?.length > 0) {
         return (
             <img
-                src={message>attachments[0].file}
+                src={message.attachments[0].file}
                 alt="message-attachment"
                 className="message-image"
                 style={{ float: 'right' }} 

74030113e3251a43beeb1e8076742c6294ef4538
noordseoostende <58941189+noordseoostende@users.noreply.github.com>
2021-03-04 15:32:33 +0100

    +30uur

--- src/components/ChatFeed.jsx
+++ src/components/ChatFeed.jsx
@@ -9,14 +9,42 @@ const ChatFeed = (props) => {
         const keys = Object.keys(messages);
         return keys.map((key, index)=> {
             const message = messages[key];
+            const lastMessageKey = index == 0 ? null : keys[index - 1];
+            const isMyMesage = userName == message.sender.username;
+            return (
+                <div key={`msg_${index}`} style={{ width: '100%' }}>
+                    <div className="message-block">
+                        {
+                            isMessage
+                            ? <MyMessage message={message} />
+                            : <TheirMessage message={message} lastMessage={messages[lastMessageKey]} />
+                        }
+                        <div className="read-receipts" style={{ marginRight: isMessage ? '18px' : '0px', marginLeft: isMyMesage ? '0px' : '68px'}}>
+                        read-receipts
+
+                        </div>
+                    </div>
+                </div>
+            );
         })
     )
-    renderMessages()
+    
+
+    if(!chat) return 'Loading...';
+
     return (
-        <div>
-            ChtFeed
+        <div className="chat-feed">
+            <div className="chat-title-container">
+                <div className="chat-title">{chat.title}</div>
+                <div className="chat-subtitle">{chat.people.map((preson) => ` ${person.person.username}`)}</div>
+            </div>
+            {renderMessages()}
+            <div style={{height: '100px' }} />
+            <div className="message-form-container">
+                <MessageForm {...props} chatId={activeChat} />
+            </div>
         </div>
-    )
+    );
 }
 
 export default ChatFeed;
